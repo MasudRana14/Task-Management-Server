cd67ab154d59af30342131dbec48e5490ce5b92e
MasudRana14 <iammasud34@gmail.com>
2023-12-24 20:42:42 +0600

    get all task data

--- index.js
+++ index.js
@@ -30,6 +30,32 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const taskCollection = client.db('TaskDB').collection('tasks');
+
+
+    // Post Task 
+    app.post('/tasks', async (req, res)=>{
+        const newTasks = req.body;
+        const result = await taskCollection.insertOne(newTasks);
+        res.send(result);
+    })
+
+    // Get Task Data 
+    app.get('/tasks', async (req, res)=>{
+      const cursor= taskCollection.find();
+      const result= await cursor.toArray();
+      res.send(result);
+    })
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
